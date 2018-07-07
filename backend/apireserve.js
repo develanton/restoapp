ee497ce870a9e25537467475ef96565319921769
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-07 14:06:56 -0500

    created the post method in reserve and some syntax changes to htmls

--- backend/apireserve.js
+++ backend/apireserve.js
@@ -1,19 +1,5 @@
-
-
-var reservations = [
-
-    {
-        "name": "Antonio",
-        "Phone number": 5552333,
-        "email": "dude@simon.com",
-        "unique id": 4545445
-    }
-
-];
-
-
-  module.exports = function(app){
-      app.get("/api/reserve", function(req, res) {
-       return res.json(reservations);
-     });
-   }
+module.exports = function(app){
+    app.get("/api/reserve", function(req, res) {
+     return res.json(reservations);
+   });
+ }
