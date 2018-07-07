87a980b16c5a0d145fa4644466b478d6d70c4270
Antonio Garza <develanton@gmail.com>
2018-07-07 11:49:46 -0500

    added ex. for adding reserved

---
+++ js/reserve.js
@@ -0,0 +1,12 @@
+var waitList = [];
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+    var newcharacter = req.body;
+  
+    console.log(newcharacter);
+  
+    characters.push(newcharacter);
+  
+    res.json(newcharacter);
+  });
